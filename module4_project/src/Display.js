dcb66c7205386911bfacd3e47a258bed15724cea
15797vishal <115143001+15797vishal@users.noreply.github.com>
2023-03-04 20:54:41 +0530

    changes

---
+++ module4_project/src/Display.js
@@ -0,0 +1,14 @@
+import React from 'react'
+ import'./App.js';
+ import 'bootstrap/dist/css/bootstrap.min.css'; 
+const Display = () => { 
+
+
+  return (
+    <div  className="col-md-12"> 
+    
+    </div> 
+  )
+}
+
+export default Display
