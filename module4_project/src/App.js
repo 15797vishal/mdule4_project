8f65c141173c1e446df067648fd1992de7f0be70
15797vishal <115143001+15797vishal@users.noreply.github.com>
2023-03-04 13:15:08 +0530

    new add in js

--- module4_project/src/App.js
+++ module4_project/src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     happy coding !
+     happy coding ! 
+     <h2>this is for new coding and add new things in this code <br></br> 
+     so we have to ready for this and make new changes in this code </h2>
     </div>
   );
 }
